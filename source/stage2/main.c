b3eaaf97ba59f84e04e1ed50da5a772957315ee8
Petr Holčák <petr7698@gmail.com>
2023-08-09 14:10:22 +0200

    Really tiny optimalization inside stage1. Preparing for big changes!

--- source/stage2/main.c
+++ source/stage2/main.c
@@ -2,12 +2,15 @@
 #include <stdnoreturn.h>
 #include <screen/vga.h>
 
+/*
+TODO: Make printf something like this (printf will take function pointer as argument that will output character)
+https://github.com/brainboxdotcc/retro-rocket/blob/d8d19bd52a809ffed2705aed60380a84cfe0ec84/src/printf.c#L18
+*/
+
 noreturn void main(void) {
 	vga_clear();
 	vga_cursor_set(0);
-	for (uint32_t i = 0; i < 24; i++) {
-		printf("Iteration %u!\n", i);
-	}
+	printf("Hello, %d!\n", 0x1234);
 	__asm__ volatile ("cli");
 	while (1);
 }
