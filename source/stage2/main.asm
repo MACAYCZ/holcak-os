3d42625fe326fb3e7de45ef954b7f3b00b6f9ff1
Petr Holčák <petr7698@gmail.com>
2023-08-10 09:35:19 +0200

    Memory detection is working.

--- source/stage2/main.asm
+++ source/stage2/main.asm
@@ -6,21 +6,23 @@ start_16:
 	mov bp, stack + stack_size
 	mov sp, bp
 
-	; Print debug message
-	mov si, .data_0
-	call puts
-
 	; Initialize memory
 	call memory_init
+	mov si, .data_0
+	call puts
+	mov ax, [memory_blocks_size]
+	call putx16
+	call putel
 
-	; TODO: Jump to stage3
+	; TODO: Protected mode, load stage3
 	cli
 	hlt
 
-.data_0: db "Debug: Entered stage2!", 0x0D, 0x0A, 0x00
+.data_0: db "Memory blocks received: 0x", 0x00
 
 %include "disk.inc"
 %include "puts.inc"
+%include "printf.inc"
 %include "memory.inc"
 
 stack_size:  equ 0x100
