847eada4fb625a664383c9248e0d61f181597096
Petr Holčák <petr7698@gmail.com>
2023-08-10 09:52:48 +0200

    Added some more comments

--- source/stage2/main.asm
+++ source/stage2/main.asm
@@ -8,26 +8,16 @@ start_16:
 
 	; Initialize memory
 	call memory_init
-	mov si, .data_0
-	call puts
-	mov ax, [memory_blocks_size]
-	call putx16
-	call putel
 
 	; TODO: Protected mode, load stage3
 	cli
 	hlt
-
-.data_0: db "Memory blocks received: 0x", 0x00
 
 %include "disk.inc"
 %include "puts.inc"
 %include "memory.inc"
 
 stack_size:  equ 0x100
-blocks_size: equ 0x100
-
 section .bss
 	stack:  resb stack_size
-	
 section .text
